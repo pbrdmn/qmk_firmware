b64110a9bf59d28dd1e1be93332857e22c8d7637
Philip Boardman <dustykeyboard@brd.mn>
2018-06-06 17:15:55 +1000

    Rearrange symbols to move shifted keys away from mod-tap areas

--- keyboards/handwired/xxxiv/keymaps/default/keymap.c
+++ keyboards/handwired/xxxiv/keymaps/default/keymap.c
@@ -17,27 +17,26 @@ extern keymap_config_t keymap_config;
 /* MOD-TAP Keys */
 #define CT_Z CTL_T(KC_Z)
 #define CT_TAB CTL_T(KC_TAB)
-#define CT_UNDS CTL_T(KC_UNDS)
+#define CT_SCLN CTL_T(KC_SCLN)
 #define CT_BSLS CTL_T(KC_BSLS)
-#define CT_SLSH CTL_T(KC_SLSH)
 
 #define AL_X ALT_T(KC_X)
 #define AL_DOT ALT_T(KC_DOT)
-#define AL_MINS ALT_T(KC_MINS)
+#define AL_EQL ALT_T(KC_EQL)
 #define AL_RBRC ALT_T(KC_RBRC)
 #define AL_GT ALT_T(KC_GT)
 
 #define GU_C GUI_T(KC_C)
 #define GU_COMM GUI_T(KC_COMM)
-#define GU_PLUS GUI_T(S(KC_PLUS))
+#define GU_MINS GUI_T(S(KC_MINS))
 #define GU_LBRC GUI_T(KC_LBRC)
 #define GU_LT GUI_T(KC_LT)
 
 #define SH_SPC SFT_T(KC_SPC)
 #define SH_A SFT_T(KC_A)
 #define SH_QUOT SFT_T(S(KC_QUOT))
-#define SH_EXLM SFT_T(S(KC_EXLM))
-#define SH_RPRN SFT_T(KC_RPRN)
+#define SH_1 SFT_T(S(KC_1))
+#define SH_0 SFT_T(KC_0)
 #define SH_END SFT_T(KC_END)
 
 /* Modified Keys */
@@ -45,7 +44,7 @@ extern keymap_config_t keymap_config;
 #define GUI_X LGUI(KC_X)
 #define GUI_C LGUI(KC_C)
 #define GUI_V LGUI(KC_V)
-#define GUI_TAB LGUI(KC_TAB)
+#define GUI_A LGUI(KC_A)
 
 /* TRNS */
 #define __ KC_TRNS
@@ -60,15 +59,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   
   [_NUM] = KEYMAP(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_UNDS, KC_MINS, KC_PLUS, KC_EQL,  KC_SCLN, KC_COLN, KC_QUES, KC_LBRC, KC_RBRC, KC_BSLS,
+    SH_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    SH_0,
+    CT_SCLN, AL_EQL,  GU_MINS, KC_PLUS, KC_UNDS, KC_COLN, KC_QUES, KC_LBRC, KC_RBRC, KC_BSLS,
                                __,      __,      __,      __
   ),
 
   [_SYM] = KEYMAP(
     KC_GRV,  KC_TILD, __,      RESET,    KC_MUTE, KC_VOLD, KC_VOLU, KC_MRWD, KC_MPLY, KC_MFFD,
-    GUI_TAB, KC_CAPS, KC_DEL,  __,       KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
+    GUI_A,   KC_CAPS, KC_DEL,  __,       KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
     GUI_Z,   GUI_X,   GUI_C,   GUI_V,    __,      __,      __,      KC_LCBR, KC_RCBR, KC_SLSH,
                                __,       __,      __,      __
   ),
