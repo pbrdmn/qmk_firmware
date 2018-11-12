a202b4e8d191065d4acd6bdd9859a82588bffed5
Philip Boardman <dustykeyboard@brd.mn>
2018-11-12 14:57:34 +1100

    remove blank line

--- keyboards/handwired/xxxiv/keymaps/default/keymap.c
+++ keyboards/handwired/xxxiv/keymaps/default/keymap.c
@@ -44,5 +44,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Z,   KC_X,   KC_C,   KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM,  KC_DOT,   TG(4),
                             KC_ESC,  KC_BSPC, KC_SPC,  KC_ENT
   ),
-
 };
