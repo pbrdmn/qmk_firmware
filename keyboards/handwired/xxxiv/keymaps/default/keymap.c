8748ebbbc9b45c5a7caf24263623e5bc04fadf8b
Philip Boardman <dustykeyboard@brd.mn>
2018-11-14 20:33:37 +1100

    Mouse keys

--- keyboards/handwired/xxxiv/keymaps/default/keymap.c
+++ keyboards/handwired/xxxiv/keymaps/default/keymap.c
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         TG(2),        KC_BSPC,  KC_SPC,   TG(3)
   ),
 
-  // Function mode - all the 'F' keys, duplicate navigation arrows, macros 8-14
+  // Function mode - all the 'F' keys, mouse keys, macros 8-14
   [3] = KEYMAP(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-    KC_F11,  KC_F12,  KC_F13,  KC_F14,   KC_HOME,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_END,
-    M_8,     M_9,     M_10,    M_11,     M_12,      M_13,     M_14,     KC_LCBR,  KC_RCBR,  KC_SLSH,
+    KC_F11,  KC_F12,  KC_F13,  KC_F14,   KC_BTN1,   KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,  KC_BTN2,
+    M_8,     M_9,     M_10,    M_11,     KC_BTN3,   KC_BTN1,  KC_BTN2,  KC_LCBR,  KC_RCBR,  KC_PIPE,
                                TG(3),    TG(2),     KC_SPC,   TG(3)
   ),
 
