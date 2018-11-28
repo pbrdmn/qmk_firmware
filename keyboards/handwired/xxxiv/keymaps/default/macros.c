c69f987ace596235af8c6a2294d560fed1411b02
Philip Boardman <dustykeyboard@brd.mn>
2018-11-28 14:01:17 +1100

    Macros

--- keyboards/handwired/xxxiv/keymaps/default/macros.c
+++ keyboards/handwired/xxxiv/keymaps/default/macros.c
@@ -11,62 +11,28 @@ enum custom_keycodes {
   M_4,
   M_5,
   M_6,
-  M_7,
-  M_8,
-  M_9,
-  M_10,
-  M_11,
-  M_12,
-  M_13,
-  M_14
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if (record->event.pressed) {
     switch(keycode) {
       case M_1:
-        SEND_STRING("Testing keyboard macro 1");
+        SEND_STRING("Macro");
         return false;
       case M_2:
-        SEND_STRING("Testing keyboard macro 2");
+        SEND_STRING("Macro");
         return false;
       case M_3:
-        SEND_STRING("Testing keyboard macro 3");
+        SEND_STRING("Macro");
         return false;
       case M_4:
-        SEND_STRING("Testing keyboard macro 4");
+        SEND_STRING("Macro");
         return false;
       case M_5:
-        SEND_STRING("Testing keyboard macro 5");
+        SEND_STRING("http://www.keyboard-layout-editor.com/#/gists/bcad6264dffcbd81bf6376ffe14ca149");
         return false;
-
       case M_6:
-        SEND_STRING("Testing keyboard macro 6");
-        return false;
-      case M_7:
-        SEND_STRING("Testing keyboard macro 7");
-        return false;
-      case M_8:
-        SEND_STRING("Testing keyboard macro 8");
-        return false;
-      case M_9:
-        SEND_STRING("Testing keyboard macro 9");
-        return false;
-      case M_10:
-        SEND_STRING("Testing keyboard macro 10");
-        return false;
-
-      case M_11:
-        SEND_STRING("Testing keyboard macro 11");
-        return false;
-      case M_12:
-        SEND_STRING("Testing keyboard macro 12");
-        return false;
-      case M_13:
-        SEND_STRING("Testing keyboard macro 13");
-        return false;
-      case M_14:
-        SEND_STRING("Testing keyboard macro 14");
+        SEND_STRING("https://github.com/dustykeyboard/qmk_firmware/tree/keyboard/xxxiv/keyboards/handwired/xxxiv");
         return false;
     }
   }
